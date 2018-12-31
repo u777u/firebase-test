c8fc27eb2185541a47ede78e7f597058db6b1c6d
nyancodeid <ryandevstudio@gmail.com>
2018-12-31 23:15:51 +0700

    update

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var i = 0;
 
 document.addEventListener('deviceready', function () {
-    cordova.plugins.firebase.analytics.setCurrentScreen("StellarFaucet - Faucet");
-    cordova.plugins.firebase.analytics.setUserId("NyanDev");
+    window.FirebasePlugin.setCurrentScreen("StellarFaucet - Faucet");
+    window.FirebasePlugin.setUserId("NyanDev");
+    window.FirebasePlugin.setAnalyticsCollectionEnabled(true);
 
 });
 
 document.getElementById('click-button').addEventListener('onclick', function() {
     i++;
-    
-    cordova.plugins.firebase.analytics.logEvent("faucetClick", {
+
+    window.FirebasePlugin.logEvent("faucetClick", {
         reward: Math.round(Math.random * 1e4),
         count: i
     });
